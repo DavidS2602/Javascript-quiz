f573345303b394a0a51526d0ccc72ed2e93a1054
David Gaspar <davsangas@hotmail.com>
2023-09-02 21:21:38 -0600

    Feat: State of Questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import './App.css'
 import { Container, Stack, Typography } from '@mui/material'
 import JavascriptLogo from './JavascriptLogo'
+import Start from './Start'
+import useQuestionsStore from './store/questions'
 
 function App() {
-
+  const questions = useQuestionsStore((state) => state.questions)
+  console.log(questions)
   return (
     <main>
       <Container maxWidth="sm">
-        <Stack>
+        <Stack direction='row' gap={2} alignItems='center' justifyContent='center'>
           <JavascriptLogo />
-          <Typography variant="h1">
-            App
+          <Typography variant="h2" component='h1'>
+            Javascript Quiz
           </Typography>
         </Stack>
+        {questions.length === 0 ? <Start /> : <p>Juego</p>}
       </Container>
     </main>
   )
---
+++ src/Start.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import useQuestionsStore from "./store/questions";
+
+const Start = () => {
+    const fetchQuestions = useQuestionsStore(state => state.fetchQuestions)
+    const handleClick = () => {
+        fetchQuestions(5)
+    }
+    return (
+        <Button onClick={handleClick} variant="container">Empezar</Button>
+    );
+}
+
+export default Start;
